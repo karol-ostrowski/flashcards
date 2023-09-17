d80dcef96d264e6fa374ef849ffd00f7c3106478
karol <karolostrowski111@gmail.com>
2023-09-17 23:01:22 +0200

    commit

---
+++ flashcards/main.py
@@ -0,0 +1,17 @@
+#app written because i don't think i like any existing flashcard app
+
+import os
+from pathlib import Path
+#import words
+
+
+current_path = os.getcwd()
+words_folder_path = os.path.join(current_path + 'words')
+
+with open(words_folder_path, 'r') as f:
+    list = os.listdir()
+    for file in list:
+        print(file)
+
+#note to self: czy jak zimportuje obiekt ktory ma zimporotwany modul to on tez bedzie zimportowany
+#asdsdasd
