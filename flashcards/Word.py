3f7854c1f685c97e64bec90823049cdca91bd625
karol <karolostrowski111@gmail.com>
2023-09-19 21:30:21 +0200

    possible to add words and see list of em from csv

--- flashcards/Word.py
+++ flashcards/Word.py
@@ -1,30 +1,24 @@
-import pickle
 import os
-from pathlib import Path
 import pandas as pd
 
-class Word():
-    def __init__(self, word_in_english = str(), word_in_norwegian = str()):
+def AddWord():
+        if not os.path.exists('words.csv'):
+            with open('words.csv', 'w') as file:
+                file.write("word in norwegian,word in english,times answered,times answered correctly")
         
-        self.word_in_english = word_in_english
-        self.word_in_norwegian = word_in_norwegian
-        self.times_answered = 0
-        self.times_answered_correctly = 0
-        self.id = 1 #needs work
-    def ListOfWords():
-        try:
-            listdir = os.listdir()
+        os.system('cls')
+        df_words = pd.read_csv('words.csv')
+        word_in_norwegian = input("adding a word\ntype the word in norwegian: ")
+        os.system('cls')
+        word_in_english = input("adding a word\ntype the word in english: ")
+        os.system('cls')
+        new_word = {'word in norwegian' : word_in_norwegian, 'word in english' : word_in_english, 'times answered' : 0, 'times answered correctly' : 0}
+        df_words.loc[len(df_words)] = new_word
+        df_words.to_csv('words.csv', index=False)
 
-"""           
-    def AddWord():
-        try:
-            os.system('cls')
-            print("Type the word in English:")
-            new_word_eng = input()
-            os.system('cls')
-            print("Type the word in Norwegian:")
-            new_word_no = input()
-
-                if
-                new_word = Word(new_word_eng, new_word_no)
-                """
+def GetListOfWords():
+    if not os.path.exists('words.csv'):
+        input("database of words is empty, press enter to continue ")
+    else:
+         df_words = pd.read_csv('words.csv')
+         print(df_words)
--- flashcards/main.py
+++ flashcards/main.py
@@ -1,19 +1,5 @@
 #app written because i don't think i like any existing flashcard app
 
-import os
-from pathlib import Path
-#import Word
+import Word
 
-
-current_path = os.getcwd()
-words_folder_path = os.path.join(current_path, 'words')
-print(words_folder_path)
-
-if os.path.exists(words_folder_path) and os.path.isdir(words_folder_path):
-    for file in os.listdir(words_folder_path):
-        print(file)
-else:
-    os.makedirs(words_folder_path)
-
-
-#note to self: czy jak zimportuje obiekt ktory ma zimporotwany modul to on tez bedzie zimportowany
+Word.GetListOfWords()
