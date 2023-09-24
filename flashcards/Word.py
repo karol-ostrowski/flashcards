210249ff179e778a7660b8bc1d8c326da52ca881
karol <karolostrowski111@gmail.com>
2023-09-24 21:59:04 +0200

    made fixes, almost done edit/remove method

--- flashcards/Word.py
+++ flashcards/Word.py
@@ -1,24 +1,78 @@
 import os
 import pandas as pd
+import time
 
 def AddWord():
-        if not os.path.exists('words.csv'):
+        if not os.path.exists('words.csv') or pd.read_csv('words.csv').shape[0] == 0:
             with open('words.csv', 'w') as file:
                 file.write("word in norwegian,word in english,times answered,times answered correctly")
-        
-        os.system('cls')
+
         df_words = pd.read_csv('words.csv')
         word_in_norwegian = input("adding a word\ntype the word in norwegian: ")
         os.system('cls')
+    
+        if word_in_norwegian in df_words['word in norwegian'].values:
+             input("word already exists in the database, press enter to continue ")
+             return
+        
         word_in_english = input("adding a word\ntype the word in english: ")
         os.system('cls')
         new_word = {'word in norwegian' : word_in_norwegian, 'word in english' : word_in_english, 'times answered' : 0, 'times answered correctly' : 0}
         df_words.loc[len(df_words)] = new_word
         df_words.to_csv('words.csv', index=False)
 
+def EditOrRemove():
+    try:
+        df_words = pd.read_csv('words.csv')
+    except:
+        input("database is empty or doesnt exist, press enter to continue...")
+        return
+    loopin = 0
+    while loopin < 1:
+        word_to_edit_or_remove = input("which word do you want to edit/remove?\ntype 'show list' to check the list of existing words or 'exit' to exit\n")
+
+        if word_to_edit_or_remove == 'show list':
+            GetListOfWords()
+        elif word_to_edit_or_remove == 'exit':
+            break
+        elif word_to_edit_or_remove in df_words['word in norwegian'].values:
+            while True:
+                os.system('cls')
+                edit_or_remove = input("1 - edit\n2 - remove\n")
+                if edit_or_remove == '1':
+                    os.system('cls')
+                    edited_word_in_norwegian = input("new spelling of the word in norwegian:\n")
+                    os.system('cls')
+                    edited_word_in_english = input("new spelling of the word in english:\n")
+                    df_words.loc[df_words['word in norwegian'] == edit_or_remove, ['word in norwegian', 'word in english']] = [edited_word_in_norwegian, edited_word_in_english]
+                    df_words.to_csv('words.csv', index=False)
+                    loopin = 1
+                    break
+                elif edit_or_remove == '2':
+                    os.system('cls')
+                    df_words.drop(df_words[df_words['word in norwegian'] == word_to_edit_or_remove].index) #doesnt delete chosen word, i think this line is a problem
+                    df_words.to_csv('words.csv', index=False)
+                    input(f"removed {word_to_edit_or_remove}, press enter to continue...")
+                    loopin = 1
+                    break
+                else:
+                    os.system('cls')
+                    print("wrong input")
+                    time.sleep(2)
+
+        else:
+            os.system('cls')
+            input("word doesnt exist in the database, press enter to continue... ")
+
 def GetListOfWords():
-    if not os.path.exists('words.csv'):
+
+    if not os.path.exists('words.csv') or os.path.getsize == 0:
         input("database of words is empty, press enter to continue ")
     else:
-         df_words = pd.read_csv('words.csv')
-         print(df_words)
+        df_words = pd.read_csv('words.csv')
+        if df_words.shape[0] == 0:
+            input("database is empty, press enter to continue...")
+        else:
+            print(df_words)
+            input('press enter to continue...')
+        
--- flashcards/main.py
+++ flashcards/main.py
@@ -1,5 +1,22 @@
 #app written because i don't think i like any existing flashcard app
-
+import os
 import Word
+import time
 
-Word.GetListOfWords()
+while True:
+    os.system('cls')
+    menu_input = input("1 - play\n2 - add a word\n3 - check the list of words with statistics\n4 - edit or remove existing word\n5 - exit\n")
+    if menu_input == '1':
+        print("starting a round")
+    elif menu_input == '2':
+        Word.AddWord()
+    elif menu_input == '3':
+        Word.GetListOfWords()
+    elif menu_input == '4':
+        Word.EditOrRemove()
+    elif menu_input == '5':
+        break
+    else:
+        os.system('cls')
+        print("wrong input")
+        time.sleep(2)
